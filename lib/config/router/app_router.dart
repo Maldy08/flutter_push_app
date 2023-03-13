820958f32c47a0c440b4aee48f1331c5d0a33b09
Carlos Maldonado <carlosmaldonado@MacBook-Pro-de-Carlos.local>
2023-03-13 10:28:12 -0700

    first commit

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_push_app/presentation/screens/home_screen.dart';
+import 'package:go_router/go_router.dart';
+
+final appRouter = GoRouter(routes: [
+  GoRoute(
+    path: '/',
+    builder: (context, state) => const HomeScreen(),
+  )
+]);
